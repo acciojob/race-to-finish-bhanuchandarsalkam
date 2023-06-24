933182747648f4a0493710df0f63dc2ef7238aaf
bhanuchandarsalkam <133007383+bhanuchandarsalkam@users.noreply.github.com>
2023-06-24 11:53:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 window.promises = [];
-let a=new Promise((resolve)=>{
+let a=new Promise(resolve=>{
 	setTimeout(()=>{
 		resolve("a")
 	}, 1000)
 })
-let b=new Promise((resolve)=>{
+let b=new Promise(resolve=>{
 	setTimeout(()=>{
 		resolve("2")
 	}, 2000)
 })
-let c=new Promise((resolve)=>{
+let c=new Promise(resolve=>{
 	setTimeout(()=>{
 		resolve("3")
 	}, 3000)
 })
-let d=new Promise((resolve)=>{
+let d=new Promise(resolve=>{
 	setTimeout(()=>{
 		resolve("4")
 	}, 4000)
 })
-let e=new Promise((resolve)=>{
+let e=new Promise(resolve=>{
 	setTimeout(()=>{
 		resolve("5")
 	}, 5000)
 })
-let x=Promise.any([a,b,c,d,e])
-x.then((data)=>
+let x=Promise.any([a, b, c, d, e])
+x.then((data)=>{
 	const element=document.getElementById("output")
 	   element.innerText=data;
-	)
+})
 
 // Do not change the code above this
 // add your promises to the array `promises`
